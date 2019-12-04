e6d4c3b86b2f424411c41345137184f06cb3c21e
Damon Vu (bav965) <bav965@mail.usask.ca>
2019-12-04 10:51:17 -0600

    added internal documentation to InvadersCountPanel and InvadersCountFrame

--- src/remainInvadersWindow/InvadersCountPanel.java
+++ src/remainInvadersWindow/InvadersCountPanel.java
@@ -13,18 +13,45 @@ import view.GraphicsPanel;
 import java.awt.*;
 import java.util.List;
 
+/**
+ * The panel to show the number of remaining invaders and its images
+ * It is an observer of the game model in order to be notified of changes,
+ * and it accesses information from the game via the GameInfoProvider interface.
+ */
 public class InvadersCountPanel extends GraphicsPanel implements GameObserver {
+    /**
+     * The object that provides information about the game.
+     */
     private GameInfoProvider gameInfo;
+
+    /**
+     * List of all images of the invaders
+     */
     private List<String> imageNames;
+
+    /**
+     * The name of the image (in the imageName list) that is being used
+     */
     private String currentImage;
+
+    /**
+     * The index of the image (in the imageNames list) that is being used
+     */
     private int imageIndex;
+
+    /**
+     * The number of remaining invaders in the game
+     */
     private int remainInvadersCount;
 
-
+    /**
+     * Initialize the invadersCount panel, including setting it up for double buffering.
+     * @param gameInfo the source for information about the game
+     */
     public InvadersCountPanel(GameInfoProvider gameInfo) {
         this.gameInfo = gameInfo;
         setDoubleBuffered(true);
-        imageNames = PropertiesDiskStorage.getInstance().getProperties("invader");
+        imageNames = PropertiesDiskStorage.getInstance().getProperties("invader"); // list of all images of the invaders
         imageIndex = 1;
         currentImage = imageNames.get(imageIndex);
         remainInvadersCount = gameInfo.getRemainInvaders();
@@ -36,6 +63,10 @@ public class InvadersCountPanel extends GraphicsPanel implements GameObserver {
         repaint();
     }
 
+    /**
+     * Paint the image of the invaders as well as the number of remaining invaders on the panel
+     * @param g the graphics to use to paint the view
+     */
     @Override
     public synchronized void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -52,10 +83,10 @@ public class InvadersCountPanel extends GraphicsPanel implements GameObserver {
         drawImage(120, 30, 150, 150,
                 currentImage, bufferedGraphics);
 
-        bufferedGraphics.setPaint(Color.GREEN);
-        drawString(100, 250, "Remaining Invaders: "+ remainInvadersCount,
-                15, bufferedGraphics);
-
+        bufferedGraphics.setPaint(Color.RED);
+        drawString(100, 250, "Remaining Invaders: ",
+                16, bufferedGraphics);
+        drawString(285, 250, Integer.toString(remainInvadersCount),
+                25, bufferedGraphics);
     }
-
 }
